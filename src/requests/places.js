87c05277cfc53d92d62315e5a897fea117890a51
Luis <luisdiazvenero@gmail.com>
2018-05-15 10:37:42 -0500

    Componente para mostrar negocios

--- src/requests/places.js
+++ src/requests/places.js
@@ -3,17 +3,20 @@ export default {
     {
       imageUrl: '/images/place-3.jpeg',
       title: 'Desayunos el rey',
-      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address: 'Av Principal 234'
     },
     {
       imageUrl: '/images/place-1.jpeg',
       title: 'Starbucks Norte',
-      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address: 'Av Principal 234'
     },
     {
       imageUrl: '/images/place-2.jpeg',
       title: 'Pizza de amor',
-      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address: 'Av Principal 234'
     }
   ]
 }
